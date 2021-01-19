7917014a48b283c97ba7f18042e89417defa409d
parkingmechanic <53428480+notuneekElf@users.noreply.github.com>
2021-01-19 19:55:43 +0530

    Add files via upload
    commented out relevant information in the py file

---
+++ stocks.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import quandl
+import math
+import time
+import numpy as np
+from sklearn import preprocessing,svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+from matplotlib import style
+import datetime
+#imported the necessary libraries, take care with the quandl library apparently Quandl isn't a thing and 
+#sk learn is used for a couple of things the mse of our model, train_test_split cross validates and our model of choice is also courtesy of scikitlearn
+#use your quandl token below
+auth_tok="#######"
+
+df=quandl.get("EOD/AAPL", trim_start="1999-04-30", trim_end="2020-12-31", authtoken=auth_tok)
+#data preprocessing because there are many redundant columns in the dataframe so 
+
+df=df[['Adj_Open','Adj_Close','Adj_High','Adj_Low','Adj_Volume']]
+df['HL_PCT']=(df['Adj_High']-df['Adj_Low']/df['Adj_Close'])*100
+df['PCT_change']=(df['Adj_Open']-df['Adj_Close']/df['Adj_Open'])*-100.0
+df=df[['Adj_Close','Adj_Volume','HL_PCT','PCT_change']]
+forecasting='Adj_Close'
+#nan cleaning hehe
+df.fillna(-99.999)
+#this is the potential range of values the model will predict, here at 10%
+forecasted=int(math.ceil(0.10*len(df)))
+df['label']=df[forecasting].shift(-forecasted)
